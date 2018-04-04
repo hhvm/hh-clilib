008132b90ab5f3c76f820d5de36c26e4a555cd9e
Fred Emmott <fe@fb.com>
2018-04-04 11:05:40 -0700

    Iniitial commit

---
+++ src/CLIOptions/CLIOptionFlag.php
@@ -0,0 +1,49 @@
+<?hh
+/**
+ * Copyright (c) 2017, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional
+ * grant of patent rights can be found in the PATENTS file in the same
+ * directory.
+ */
+
+namespace Facebook\HHAST\__Private\CLIOptions;
+
+final class CLIOptionFlag extends CLIOption {
+  const type TSetter = (function():void);
+
+  public function __construct(
+    private self::TSetter $setter,
+    string $help_text,
+    string $long,
+    ?string $short,
+  ) {
+    parent::__construct($help_text, $long, $short);
+  }
+
+  public function set(): void {
+    $setter = $this->setter;
+    $setter();
+  }
+
+  <<__Override>>
+  public function apply(
+    string $as_given,
+    ?string $value,
+    vec<string> $argv,
+  ): vec<string> {
+    if ($value !== null) {
+      \fprintf(
+        \STDERR,
+        "'%s' specifies a value, however values aren't supported for that ".
+        "option.\n",
+        $as_given,
+      );
+      exit(1);
+    }
+    $this->set();
+    return $argv;
+  }
+}
---
+++ src/CLIOptions/CLIOptionWithRequiredValue.php
@@ -0,0 +1,53 @@
+<?hh
+/**
+ * Copyright (c) 2017, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the "hack" directory of this source tree. An additional
+ * grant of patent rights can be found in the PATENTS file in the same
+ * directory.
+ */
+
+namespace Facebook\HHAST\__Private\CLIOptions;
+
+use namespace HH\Lib\{C, Vec};
+
+final class CLIOptionWithRequiredValue extends CLIOption {
+  const type TSetter = (function(string):void);
+  public function __construct(
+    private self::TSetter $setter,
+    string $help_text,
+    string $long,
+    ?string $short,
+  ) {
+    parent::__construct($help_text, $long, $short);
+  }
+
+  public function set(string $value): void {
+    $setter = $this->setter;
+    $setter($value);
+  }
+
+  <<__Override>>
+  public function apply(
+    string $as_given,
+    ?string $value,
+    vec<string> $argv,
+  ): vec<string> {
+    if ($value === null) {
+      if (C\is_empty($argv)) {
+        \fprintf(
+          \STDERR,
+          "option '%s' requires a value\n",
+          $as_given,
+        );
+        exit(1);
+      }
+      $value = C\firstx($argv);
+      $argv = Vec\drop($argv, 1);
+    }
+    $this->set($value);
+    return $argv;
+  }
+}
