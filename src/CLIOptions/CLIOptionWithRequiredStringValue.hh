e816c4de93b8f5a133a4492208186cdd612d6e33
Joel Marcey <joelm@fb.com>
2018-05-16 15:17:31 -0700

    Document CLIOptionWithRequiredStringValue (#13)

--- src/CLIOptions/CLIOptionWithRequiredStringValue.hh
+++ src/CLIOptions/CLIOptionWithRequiredStringValue.hh
@@ -12,9 +12,26 @@ namespace Facebook\CLILib\CLIOptions;
 
 use namespace HH\Lib\{C, Vec};
 
+/**
+ * Creates a CLI option that requrires an associated string value.
+ *
+ * e.g., `--outputDir /tmp/dir`, where `/tmp/dir` would be the associated
+ * string.
+ *
+ * @see CLIOption
+ */
 final class CLIOptionWithRequiredStringValue extends CLIOptionWithRequiredValue {
   const type TSetter = (function(string):void);
 
+  /**
+   * Creates the option with the required string.
+   *
+   * @param $setter A callable that can be used to set a property in the class
+   * you are using to implement your CLI.
+   * @param $help_text The text shown when the user provides the `--help` flag.
+   * @param $long The long name for the flag. e.g., `--output`.
+   * @param short An optional short name for the flag. e.g., `-o`.
+   */
   public function __construct(
     private self::TSetter $setter,
     string $help_text,
@@ -24,6 +41,14 @@ final class CLIOptionWithRequiredStringValue extends CLIOptionWithRequiredValue 
     parent::__construct($help_text, $long, $short);
   }
 
+  /**
+   * Set the option with the provided value, using the setter provided when
+   * the option was created.
+   *
+   * @param $as_given The option as specified by the user. Usually matches
+   *   `getShort()` or `getLong()`
+   * @param $value The value specified by the user.
+   */
   protected function set(string $_as_given, string $value): void {
     $setter = $this->setter;
     $setter($value);
