001ca3369d72ebb78df198add100a53c400e8fb5
Joel Marcey <joelm@fb.com>
2018-05-16 14:02:24 -0700

    Document OutputInterface (#7)

--- src/OutputInterface.hh
+++ src/OutputInterface.hh
@@ -11,7 +11,26 @@
 
 namespace Facebook\CLILib;
 
+/**
+ * This interface provides a mechanism for providing output from the CLI.
+ *
+ * The Hack CLI provides default mechanisms to access stdout and stderr for
+ * displaying output. And those are where this interface will usually be
+ * connectd. There are other possiblities as well, and this interface allows
+ * you to define those - e.g., an output mock for unit testing.
+ *
+ * @see CLIBase
+ */
 interface OutputInterface {
+  /**
+   * The function that is used to write the specified string to the CLI output.
+   *
+   * @return an integer representing the number of bytes written or a code of
+   * failure.
+   */
   public function write(string $_): int;
+  /**
+   * Returns `true` if we have hit the end of the file for the stream.
+   */
   public function isEof(): bool;
 }
