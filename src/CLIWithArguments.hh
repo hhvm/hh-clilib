00f1a6b3f11558f3659f27eea3659f64e5f14541
Joel Marcey <joelm@fb.com>
2018-05-16 09:01:43 -0700

    Document CLIWithArguments

--- src/CLIWithArguments.hh
+++ src/CLIWithArguments.hh
@@ -11,7 +11,30 @@
 
 namespace Facebook\CLILib;
 
+/**
+ * A class to represent a CLI that has arguments associated with it.
+ *
+ * Arguments are strings passed to the CLI after all flags and options are
+ * provided.
+ *
+ * e.g., here the argument would be `../hh-clilib`
+ *
+ * ```
+ * hhvm bin/hh-apidoc -o /tmp ../hh-clilib
+ *
+ * This class adds one additional function to the parent `CLIBase`.
+ *
+ * @see CLIBase
+ */
 abstract class CLIWithArguments extends CLIBase {
+  /**
+   * For arguments that are optional to the CLI, this function will provide
+   * their help text.
+   *
+   * The default return is `ARGUMENT` - this can be overridden for more detailed
+   * information.
+   *
+   */
   public static function getHelpTextForOptionalArguments(): string {
     return 'ARGUMENT';
   }
