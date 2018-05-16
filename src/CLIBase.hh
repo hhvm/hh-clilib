ba4b769c1dbaf4ef7cf54658717d9820f51a35c7
Joel Marcey <joelm@fb.com>
2018-05-16 13:59:07 -0700

    Document CLIBase (#1)

--- src/CLIBase.hh
+++ src/CLIBase.hh
@@ -14,19 +14,105 @@ namespace Facebook\CLILib;
 use namespace Facebook\TypeAssert;
 use namespace HH\Lib\{C, Dict, Str, Vec};
 
+/**
+ * This class contains all of the core functionality for using the Hack CLI
+ * library. It will never be instantiated directly, but instead, will invoked
+ * through a call to its `main` function.
+ *
+ * The Hack CLI has the notion of options and arguments.
+ *
+ * options: An instance of the Hack CLI has a limited set of options that can be
+ * used to customize the experience of its execution. For example, many CLIs
+ * will have a `--help` option that can be passed to it. In the case of th Hack
+ * CLI, `--help` does not even need to be explictly specified; it always exists.
+ *
+ * arguments: These are passed to the CLI after all the options are provided.
+ * In general, the arguments are what is being executed upon.
+ *
+ * All non-abstract types extending this class (or any of its children), will
+ * implement the definition of the abstract function `mainAsync`, which is
+ * called by the `main` function. `mainAsync` will provide all the custom
+ * execution when your CLI commands are invoked.
+ *
+ * ```Hack
+ * // MyCLI.hh
+ * <?hh // strict
+ * final class MyCLI extends CLIBase {
+ *   <<__Override>>
+ *   public async function mainAsync(): Awaitable<int> {
+ *     // Custom CLI handling
+ *   }
+ * }
+ * ```
+ *
+ * ```Hack
+ * <?hh // not strict because of top-level statements.
+ * // RunMyCLI.hh
+ * MyCLI::main(); // calls the main function of CLIBase
+ * ```
+ */
 <<__ConsistentConstruct>>
 abstract class CLIBase {
   private vec<string> $arguments = vec[];
 
+  /**
+   * Returns the list of options that is supported by the CLI.
+   */
   abstract protected function getSupportedOptions(
   ): vec<CLIOptions\CLIOption>;
 
+  /**
+   * This is implemented by all implementations of the Hack CLI and provides
+   * all of the custom execution that will occur when that CLI is invoked.
+   *
+   * @return  An integer representing an exit code for the process. 0 is the
+   * standard for success.
+   */
   abstract public function mainAsync(): Awaitable<int>;
 
+  /**
+   * Returns the list of all the arguments that were passed in any given use
+   * of the CLI, including the process, flags, options and other arguments.
+   *
+   * In the following example:
+   *
+   * ```
+   * hhvm bin/hh-apidoc -o /tmp ../hh-clilib
+   * ```
+   *
+   * the `vec` returned would be:
+   *
+   * ```
+   * vec(4) {
+   *   string(13) "bin/hh-apidoc"
+   *   string(2) "-o"
+   *   string(4) "/tmp"
+   *   string(18) "../hh-clilib"
+   * }
+   * ```
+   */
   final public function getArgv(): vec<string> {
     return $this->argv;
   }
 
+  /**
+   * Returns the list of the arguments that were passed to the CLI after
+   * all of the options have been parsed.
+   *
+   * In the following example:
+   *
+   * ```
+   * hhvm bin/hh-apidoc -o /tmp ../hh-clilib
+   * ```
+   *
+   * the `vec` returned would be:
+   *
+   * ```
+   * vec(1) {
+   *  string(18) "../hh-clilib/"
+   * }
+   * ```
+   */
   final protected function getArguments(): vec<string> {
     invariant(
       ($_ = $this) instanceof CLIWithArguments,
@@ -35,6 +121,13 @@ abstract class CLIBase {
     return $this->arguments;
   }
 
+  /**
+   * Determines whether your current terminal supports colors.
+   *
+   * This function will automatically return `true` in some non-interactive
+   * cases, such as non-interactive contexts provided in continuous integration
+   * (CI) systems.
+   */
   protected function supportsColors(): bool {
     static $cache;
 
@@ -81,6 +174,11 @@ abstract class CLIBase {
     return false;
   }
 
+  /**
+   * Determines whether the current terminal is in interactive mode.
+   *
+   * In general, this is `true` if the user is directly typing into stdin.
+   */
   protected function isInteractive(): bool {
     static $cache = null;
     if ($cache !== null) {
@@ -121,14 +219,38 @@ abstract class CLIBase {
     return false;
   }
 
+  /**
+   * Gets the standard process output for the current CLI.
+   *
+   * By default, this is the process standard output, or file descriptor 1.
+   */
   final protected function getStdout(): OutputInterface {
     return $this->stdout;
   }
 
+  /**
+   * Gets the standard error output for the current CLI.
+   *
+   * By default, this is the process standard error, or file descriptor 2.
+   *
+   * This is usually a wrapper around stdout, and should be used instead of
+   * direct access.
+   */
   final protected function getStderr(): OutputInterface {
     return $this->stderr;
   }
 
+  /**
+   * This is usually the main entry point to create an instance of a Hack CLI.
+   * By default, the output is to stdout and errors are written to stderr. But
+   * those can be overriden by the actual concrete instance of this abstract
+   * class.
+   *
+   * This function does not return anything. Instead `exit()` is called with an
+   * exit code for success and failure.
+   *
+   * @see CLIBase::mainAsync
+   */
   final public static function main(): noreturn {
     try {
       $responder = new static(
@@ -146,6 +268,13 @@ abstract class CLIBase {
     }
   }
 
+  /**
+   * This creates a new instance of the Hack CLI.
+   *
+   * This will generally be called indirectly via a call to `main()`. However,
+   * a direct call to this constructor may be useful in certain cases, such as
+   * mocks and unit tests.
+   */
   public function __construct(
     private vec<string> $argv,
     private OutputInterface $stdout,
@@ -246,6 +375,12 @@ abstract class CLIBase {
     );
   }
 
+  /**
+   * Displays the help and usage information for this CLI.
+   *
+   * The help information is automatically generated. The default generation
+   * can be overridden - but generally not necessary.
+   */
   public function displayHelp(OutputInterface $out): void {
     $usage = 'Usage: '.C\firstx($this->argv);
 
