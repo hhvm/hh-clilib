f491dea80ec36ef3ef4749ddeeeafc7eaa671886
Joel Marcey <joelm@fb.com>
2018-05-16 14:23:34 -0700

    Document CLIOptionFlag (#11)

--- src/CLIOptions/CLIOptionFlag.hh
+++ src/CLIOptions/CLIOptionFlag.hh
@@ -12,9 +12,31 @@ namespace Facebook\CLILib\CLIOptions;
 
 use type Facebook\CLILib\InvalidArgumentException;
 
+/**
+ * Adds a flag option to the CLI.
+ *
+ * A flag has no associated required values associated with it.
+ *
+ * A wrapper for this class is provided in the
+ * `Facebook\CLILib\CLIOptions\flag()` function.
+ *
+ */
 final class CLIOptionFlag extends CLIOption {
   const type TSetter = (function():void);
 
+  /**
+   * Create the flag.
+   *
+   * @param $setter A callable that can be used to set a property in the class
+   * you are using to implement your CLI.
+   * @param $help_text The text shown when the user provides the `--help` flag.
+   * @param $long The long name for the flag.
+   * @param short An optional short name for the flag.
+   *
+   * @return a `CLIOption` that can then be used to call the `$setter`, etc.
+   *
+   * @see CLIOption
+   */
   public function __construct(
     private self::TSetter $setter,
     string $help_text,
@@ -24,11 +46,27 @@ final class CLIOptionFlag extends CLIOption {
     parent::__construct($help_text, $long, $short);
   }
 
+  /**
+   * The setter for the flag.
+   *
+   * The setter is provided as a callable when the flag is constructed.
+   */
   public function set(): void {
     $setter = $this->setter;
     $setter();
   }
 
+  /**
+   * Process user input, return new argv.
+   *
+   * @param $as_given - the option as specified by the user. Usually matches
+   *   `getShort()` or `getLong()`
+   * @param $value - value specified by the users (e.g. `--long=value`). If
+   *   null, it may be appropriate to take a value from $argv
+   * @param $argv - remaining unprocessed $argv
+   *
+   * @return the new $argv after this option is applied.
+   */
   <<__Override>>
   public function apply(
     string $as_given,
