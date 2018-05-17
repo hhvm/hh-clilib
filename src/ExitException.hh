147e049c52b5e757c66633306f47c3e872feb3bf
Joel Marcey <joelm@fb.com>
2018-05-17 09:47:34 -0700

    Document ExitException (#6)

--- src/ExitException.hh
+++ src/ExitException.hh
@@ -10,7 +10,23 @@
 
 namespace Facebook\CLILib;
 
+/**
+ * An `Exception` used when you want to explicitly exit the running of the CLI
+ * process and provide a specific exit code.
+ *
+ * In real usage, the exception handler is likely to call `exit()`` with the
+ * specified exit code.
+ *
+ * @see CLIException
+ */
 final class ExitException extends CLIException {
+  /**
+   * Create the exception with the exit code.
+   *
+   * Note that success via `new ExitException(0)` is valid usage. For example,
+   * you may throw this exception if you are jumping out of the CLI process
+   * because you specified a certain flag.
+   */
   public function __construct(
     int $code,
   ) {
